342c9ec6ccd75b98e39ee8d1f206532904f5971b
Alexandra <alexandra.kovacs@vtsd.com>
2024-03-19 09:26:17 -0400

    h

--- 2.1.3/pwalgorithms.py
+++ 2.1.3/pwalgorithms.py
@@ -3,14 +3,16 @@
 # get words from password dictionary file
 def get_dictionary():
   words = []
+  word = []
   dictionary_file = open("dictionary.txt")
   for line in dictionary_file:
     # store word, omitting trailing new-line
     words.append(line[:-1])
+    word.append(line[:-1])
   dictionary_file.close()
-  return words
+  return words, word
 
-'''
+
 # analyze a one-word password
 def one_word(password):
   words = get_dictionary()
@@ -18,9 +20,21 @@ def one_word(password):
   # get each word from the dictionary file
   for w in words:
     guesses += 1
+    print(w)
     if (w == password):
       return True, guesses
   return False, guesses
-  '''
+
+def two_word(password):
+  words = get_dictionary()
+  guesses = 0
+  for w in words:
+    for ws in words:
+      guesses += 1
+      myGuess = w + ws
+      print(myGuess)
+      if (w + ws) == password:
+        return True, guesses
+  return False, guesses
 
 
