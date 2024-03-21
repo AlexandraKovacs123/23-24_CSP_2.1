1d2c220f0fa913aa1f7699c6dc5e73206b686c18
Alexandra <alexandra.kovacs@vtsd.com>
2024-03-21 07:58:05 -0400

    h

--- 2.1.4/step 5.py
+++ 2.1.4/step 5.py
@@ -2,13 +2,20 @@
 #   A program creates a window on your screen using Tkinter.
 import tkinter as tk
 
+def test_my_button():
+    frame_auth.tkraise()
+    ent_password.get()
+    label.config(text=)
+
 # main window
 root = tk.Tk()
 root.wm_geometry("300x300")
 
 # create empty frame
 frame_login = tk.Frame(root)
-frame_login.grid()
+frame_auth = tk.Frame(root)
+frame_login.grid(row=0, column=0, sticky="news")
+frame_auth.grid(row=0, column=0, sticky="news")
 
 lbl_username = tk.Label(frame_login, text='Username:')
 lbl_username.pack()
@@ -16,13 +23,19 @@ lbl_username.pack()
 ent_username = tk.Entry(frame_login, bd=3)
 ent_username.pack(pady=5)
 
-lbl_username = tk.Label(frame_login, text='Password:')
-lbl_username.pack()
+lbl_password = tk.Label(frame_login, text='Password:')
+lbl_password.pack()
 
-ent_username = tk.Entry(frame_login, bd=3)
-ent_username.pack(pady=5)
+ent_password = tk.Entry(frame_login, bd=3, show="*")
+ent_password.pack(pady=5)
 
-btn_login = tk.Button(parent, text='login', command=submitForm)
+btn_login = tk.Button(frame_login, text="Login", command=test_my_button)
+btn_login.pack()
+
+frame_login.tkraise()
+
+label = tk.Label(frame_auth)
+
 
 
 root.mainloop()
